cf192cf1f812a6911e4c9734b239e0bbaad31931
Theo48-2A <114155403+Theo48-2A@users.noreply.github.com>
2022-10-29 12:07:21 +0200

    Update StatementPrinterTests.java

--- test/StatementPrinterTests.java
+++ test/StatementPrinterTests.java
@@ -12,9 +12,9 @@ public class StatementPrinterTests {
     @Test
     void testAcceptationText() {
         Map<String, Play> plays = Map.of(
-                "hamlet",  new Play("Hamlet", "tragedy"),
-                "as-like", new Play("As You Like It", "comedy"),
-                "othello", new Play("Othello", "tragedy"));
+                "hamlet",  new Play("Hamlet", TypePiecesOk.TRAGEDY),
+                "as-like", new Play("As You Like It", TypePiecesOk.COMEDY),
+                "othello", new Play("Othello", TypePiecesOk.TRAGEDY));
 
         Invoice invoice = new Invoice("BigCo", List.of(
                 new Performance("hamlet", 55),
@@ -31,9 +31,9 @@ public class StatementPrinterTests {
     @Test
     void testAcceptationHtml() {
         Map<String, Play> plays = Map.of(
-                "hamlet",  new Play("Hamlet", "tragedy"),
-                "as-like", new Play("As You Like It", "comedy"),
-                "othello", new Play("Othello", "tragedy"));
+                "hamlet",  new Play("Hamlet", TypePiecesOk.TRAGEDY),
+                "as-like", new Play("As You Like It", TypePiecesOk.COMEDY),
+                "othello", new Play("Othello", TypePiecesOk.TRAGEDY));
 
         Invoice invoice = new Invoice("BigCo", List.of(
                 new Performance("hamlet", 55),
@@ -46,31 +46,14 @@ public class StatementPrinterTests {
         verify(result);
     }
     
-    // Ce test n'a plus raison d'être car maintenant la validité d'un type de pièce se fait depuis la création de l'objet.
+   // Ce test n'est plus utile car maintenant une enum gere les types de pieces
 /*
-    @Test
-    void statementWithNewPlayTypes() {
-        Map<String, Play> plays = Map.of(
-                "henry-v",  new Play("Henry V", "history"),
-                "as-like", new Play("As You Like It", "pastoral"));
-
-        Invoice invoice = new Invoice("BigCo", List.of(
-                new Performance("henry-v", 53),
-                new Performance("as-like", 55)));
-
-        StatementPrinter statementPrinter = new StatementPrinter();
-        Assertions.assertThrows(Error.class, () -> {
-            statementPrinter.print(invoice, plays);
-        });
-    }
-*/  
-    //Maintenant on vérifie que l'on est bien un NullPointerException quand on saisit un mauvais type de pièce à la création d'un objet play
     @Test
     void statementWithNewPlayTypes() {
 
         Assertions.assertThrows(NullPointerException.class, () -> {
-            new Play("Henry V", "history");
-            new Play("As You Like It", "pastoral");
+            new Play("Henry V", TypePiecesOk.COMEDY);
+            new Play("As You Like It", TypePiecesOk.TRAGEDY);
         });
     }
 }
