5240120132b8c82a83b8b7f46fd4cd313f093834
Theo48-2A <114155403+Theo48-2A@users.noreply.github.com>
2022-10-29 12:36:50 +0200

    Update Invoice.java

--- main/Invoice.java
+++ main/Invoice.java
@@ -2,15 +2,16 @@ import java.util.*;
 
 public class Invoice {
 
-  private String customer;  //Le nom du client
+  private Customer customer;
+  //private String customer;  //Le nom du client
   private List<Performance> performances;   // La liste des performances pour ce client
 
-  public Invoice(String customer, List<Performance> performances) {
+  public Invoice(Customer customer, List<Performance> performances) {
     this.customer = customer;
     this.performances = performances;
   }
   
-  public String getCustomer(){
+  public Customer getCustomer(){
      return this.customer;
   }
   
